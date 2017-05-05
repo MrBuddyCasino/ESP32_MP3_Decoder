96808d55bf3709f7d166c317145a5d66c9b7aee0
Michael Böckling <michael@boeckling.net>
2017-05-05 22:23:00 +0200

    added cflags and include dir

---
+++ components/libfaad/component.mk
@@ -0,0 +1,3 @@
+
+COMPONENT_ADD_INCLUDEDIRS := include codebook .
+CFLAGS += -Wno-error=unused-function -Wno-error=unused-variable -Wno-error=maybe-uninitialized -Wno-error=unused-value -DHAVE_MEMCPY -DSTDC_HEADERS -DHAVE_INTTYPES_H -DHAVE_STRINGS_H -DFIXED_POINT
