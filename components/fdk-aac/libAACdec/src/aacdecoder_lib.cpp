bfde577173b493a18c907b94754feed13b799a24
Michael Böckling <michael@boeckling.net>
2017-05-09 13:35:26 +0200

    disable limiter to save memory

--- components/fdk-aac/libAACdec/src/aacdecoder_lib.cpp
+++ components/fdk-aac/libAACdec/src/aacdecoder_lib.cpp
@@ -2,7 +2,7 @@
 /* -----------------------------------------------------------------------------------------------------------
 Software License for The Fraunhofer FDK AAC Codec Library for Android
 
-© Copyright  1995 - 2015 Fraunhofer-Gesellschaft zur Förderung der angewandten Forschung e.V.
+ï¿½ Copyright  1995 - 2015 Fraunhofer-Gesellschaft zur Fï¿½rderung der angewandten Forschung e.V.
   All rights reserved.
 
  1.    INTRODUCTION
@@ -680,15 +680,14 @@ LINKSPEC_CPP HANDLE_AACDECODER aacDecoder_Open(TRANSPORT_TYPE transportFmt, UINT
     err = -1;
     goto bail;
   }
-
+  /* disable limiter to save memory
   aacDec->hLimiter = createLimiter(TDL_ATTACK_DEFAULT_MS, TDL_RELEASE_DEFAULT_MS, SAMPLE_MAX, (8), 96000);
   if (NULL == aacDec->hLimiter) {
     err = -1;
     goto bail;
-  }
+  }*/
   aacDec->limiterEnableUser = (UCHAR)-1;
   aacDec->limiterEnableCurr = 0;
-
 
 
   /* Assure that all modules have same delay */
@@ -696,7 +695,7 @@ LINKSPEC_CPP HANDLE_AACDECODER aacDecoder_Open(TRANSPORT_TYPE transportFmt, UINT
     err = -1;
     goto bail;
   }
-
+  
 bail:
   if (err == -1) {
     aacDecoder_Close(aacDec);
@@ -713,7 +712,7 @@ LINKSPEC_CPP AAC_DECODER_ERROR aacDecoder_Fill(
         )
 {
   TRANSPORTDEC_ERROR tpErr;
-  /* loop counter for layers; if not TT_MP4_RAWPACKETS used as index for only 
+  /* loop counter for layers; if not TT_MP4_RAWPACKETS used as index for only
      available layer                                                           */
   INT layer      = 0;
   INT nrOfLayers = self->nrOfLayers;
@@ -749,7 +748,7 @@ static void aacDecoder_UpdateBitStreamCounters(CStreamInfo *pSi, HANDLE_FDK_BITS
 
   /* Note: The amount of bits consumed might become negative when parsing a
      bit stream with several sub frames, and we find out at the last sub frame
-     that the total frame length does not match the sum of sub frame length. 
+     that the total frame length does not match the sum of sub frame length.
      If this happens, the transport decoder might want to rewind to the supposed
      ending of the transport frame, and this position might be before the last
      access unit beginning. */
