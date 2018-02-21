b3c235c876eba738ad0ccda6a571a51a0e0f7802
unkown12345 <32942966+unkown12345@users.noreply.github.com>
2018-02-21 23:26:38 +0100

    some lines to get icy-metaint:

--- components/http/http.c
+++ components/http/http.c
@@ -20,8 +20,8 @@
 
 
 #define TAG "http_client"
-
-
+int icymeta,httpmeta;
+char icymeta_text[64];
 /**
  * @brief simple http_get
  * see https://github.com/nodejs/http-parser for callback usage
@@ -77,7 +77,10 @@ int http_client_get(char *uri, http_parser_settings *callbacks, void *user_data)
 
     // write http request
     char *request;
-    if(asprintf(&request, "GET %s HTTP/1.0\r\nHost: %s:%d\r\nUser-Agent: ESP32\r\nAccept: */*\r\n\r\n", url->path, url->host, url->port) < 0)
+//    if(asprintf(&request, "GET %s HTTP/1.0\r\nHost: %s:%d\r\nUser-Agent: ESP32\r\nAccept: */*\r\n\r\n", url->path, url->host, url->port) < 0)
+
+	    if(asprintf(&request, "%s%s%s%s%s%s%s%s%s", "GET ", url->path, " HTTP/1.0\r\nHost: ", url->host, "\r\n","User-Agent: ESP32\r\n","Range: bytes=0-\r\n","Connection: close\r\n","Icy-MetaData: 1\r\n\r\n") < 0)
+
     {
         return ESP_FAIL;
     }
@@ -94,6 +97,7 @@ int http_client_get(char *uri, http_parser_settings *callbacks, void *user_data)
 
 
     /* Read HTTP response */
+    int i,k=0,k1=0,k2=0;httpmeta=1;icymeta=0;//icy-metaint Parser
     char recv_buf[64];
     bzero(recv_buf, sizeof(recv_buf));
     ssize_t recved;
@@ -108,6 +112,42 @@ int http_client_get(char *uri, http_parser_settings *callbacks, void *user_data)
     esp_err_t nparsed = 0;
     do {
         recved = read(sock, recv_buf, sizeof(recv_buf)-1);
+
+    //icy-metaint Parser dedect icy-metaint: for spiram_fifo.c    
+    if(k2<1000) {
+       k2++;
+         if(k!=0)  { 
+            i=k;
+            if (recv_buf[k-i] == 0x69&&k==1)  k++;   //i
+            if (recv_buf[k-i] == 0x6e&&k==2)  k++;   //n
+            if (recv_buf[k-i] == 0x74&&k==3)  k++;   //t
+            if (recv_buf[k-i] == 0x3a&&k==4)  k++;    //:
+            if (k==5) {k1=recv_buf[k-i]-0x30; k++;} //Zahl
+            k1=k1*10000+(recv_buf[k-i]-0x30)*1000;  //Zahl
+               if(k<5)   
+                  k1=0;
+                  k=0;
+         };    
+
+
+         if(k1==0)  {
+            for (i=0;i<recved;i++) {
+               if (recv_buf[i] == 0x61){k++;if(i>=recved) break;      //a
+               if (recv_buf[i+k] == 0x69){k++;if(i+k>=recved) break;  //i
+               if (recv_buf[i+k] == 0x6e){k++;if(i+k>=recved) break;  //n
+               if (recv_buf[i+k] == 0x74){k++;if(i+k>=recved) break;  //t
+               if (recv_buf[i+k] == 0x3a){k++;if(i+k>=recved) break;  //:
+               k1=recv_buf[i+k]-0x30;k++;if(i+k>=recved) break;       //Zahl
+               k1=k1*10000+(recv_buf[i+k]-0x30)*1000;k=0;
+               icymeta=k1; //icy-metaint: found
+               printf("icy-metaint: k1=%d\n",k1);
+               k2=1000;
+               break;}
+               }}}} k=0;
+            }
+         };
+
+       };
 
         // using http parser causes stack overflow somtimes - disable for now
         nparsed = http_parser_execute(&parser, callbacks, recv_buf, recved);
