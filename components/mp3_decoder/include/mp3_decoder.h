e8758f7ee4fdac0a5a34c1b3819107d80642010d
fc92 <florent.carre@free.fr>
2021-01-01 22:48:00 +0100

    protect mp3_decoder header from multiple import

--- components/mp3_decoder/include/mp3_decoder.h
+++ components/mp3_decoder/include/mp3_decoder.h
@@ -5,4 +5,8 @@
  *      Author: michaelboeckling
  */
 
+#ifndef _INCLUDE_LIBMP3_DECODER_H_
+#define _INCLUDE_LIBMP3_DECODER_H_
 void mp3_decoder_task(void *pvParameters);
+
+#endif /* _INCLUDE_LIBMP3_DECODER_H_ */
