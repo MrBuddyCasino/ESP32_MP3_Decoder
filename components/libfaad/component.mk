bff655ed5aa6618cc8e8a351781a810ccb8c9ae3
fc92 <florent.carre@free.fr>
2021-01-05 22:57:30 +0100

    remove Makefile and component.mk no more used

--- components/libfaad/component.mk
+++
@@ -1,4 +0,0 @@
-
-COMPONENT_ADD_INCLUDEDIRS := include codebook .
-# -DFIXED_POINT
-CFLAGS += -DHAVE_MEMCPY -DSTDC_HEADERS -DHAVE_INTTYPES_H -DHAVE_INTTYPES_H -DHAVE_STRINGS_H -Wno-error=unused-function -Wno-unused-function -Wno-error=unused-variable -Wno-unused-variable -Wno-error=maybe-uninitialized -Wno-maybe-uninitialized -Wno-error=unused-value -Wno-unused-but-set-variable 
--- main/component.mk
+++
@@ -1,1 +0,0 @@
-COMPONENT_EMBED_FILES := playlist.pls
